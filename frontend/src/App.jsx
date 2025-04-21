87224747a0e9024ca57cfca49f667059331f40e2
Riteshkumar12345 <ritesh.kumar3_cs22@gla.ac.in>
2025-04-21 19:03:23 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Doctors from './pages/Doctors'
-import Myappoinment from './pages/Myappoinment'
+import Myappoinment from './pages/Myappointment'
 import Myprofile from './pages/Myprofile'
 import Contact from './pages/Contact'
 import Login from './pages/Login'
 import About from './pages/About'
 import Appointment from './pages/Appointment'
 import Navbar from './component/Navbar'
+import Footer from './component/Footer'
 
 
 const App = () => {
@@ -19,14 +20,15 @@ const App = () => {
       <Route path='/' element={<Home/>} />
       <Route path='/doctor' element={<Doctors/>} />
       <Route path='/doctor/:speciality' element={<Doctors/>} />
-      <Route path='/my-appoinment' element={<Myappoinment/>} />
+      <Route path='/my-appointment' element={<Myappoinment/>} />
       <Route path='/my-profile' element={<Myprofile/>} />
       <Route path='/contact' element={<Contact/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/about' element={<About/>} />
-      <Route path='/appoinment/:docId' element={<Appointment/>} />
+      <Route path='/appointment/:docId' element={<Appointment/>} />
 
      </Routes>
+     <Footer/>
 
 </div>
   )
